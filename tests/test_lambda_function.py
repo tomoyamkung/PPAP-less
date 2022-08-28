f7082621b0fb78b3d97fc17a1e6895bb30d0bb8c
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2022-08-28 18:34:36 +0900

    :speech_balloon:ユニットテストを省略

--- tests/test_lambda_function.py
+++ tests/test_lambda_function.py
@@ -1,8 +1,2 @@
-import pytest
-
-from src.lambda_function import echo
-
-
-@pytest.mark.parametrize(("param", "expected"), [("hoge", "hoge")])
-def test_echo(param, expected):
-    assert echo(param) == expected
+def test_hoge():
+    assert True
